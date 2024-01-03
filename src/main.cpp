e28ae7faf8ce1bb8e8f4b0e39bf45d3a200a6642
wwu <wwu@topcon.com>
2024-01-03 13:21:22 +1000

    vscode compile and run

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+int main() {
+    std::string sLine;
+    std::getline(std::cin, sLine);
+
+    uint32_t nAttribute, nQuery;
+    sscanf(sLine.c_str(), "%d %d", nAttribute, nQuery);
+
+    return 0;
+}
